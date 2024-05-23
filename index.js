9a6ef38789587caf8d7f79de484dc55fa2c04ad1
237harsh <92569918+237harsh@users.noreply.github.com>
2024-05-24 04:45:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const connectDB = require('./middlewares/db')
 const app = express()
 var cors = require('cors');
 connectDB()
+// Allow requests from your frontend origin
 const corsOptions = {
   origin: 'https://healthy-frontend-neon.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
   optionsSuccessStatus: 200,
 };
 
+// Apply the CORS middleware before defining routes
 app.use(cors(corsOptions));
+
 const port = 5000
 app.use(express.json())
 app.use('/api/auth',require('./routes/auth'))

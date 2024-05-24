db9922c33b737d33a7358c91dbd3f69038952c5b
237harsh <92569918+237harsh@users.noreply.github.com>
2024-05-24 05:34:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,11 @@ connectDB()
 const cors = require('cors');
 const app = express();
 
-const corsOptions = {
-  origin: 'https://paramvs.site', // Replace with your actual Vercel domain
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-  optionsSuccessStatus: 200,
-};
 
-app.use(cors(corsOptions));
-
-// Ensure preflight requests are handled
-app.options('*', cors(corsOptions));
 
 const port = 5000
 app.use(express.json())
+app.use(cors());
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/data',require('./routes/symptom'))
 app.get('/', (req, res) => {
